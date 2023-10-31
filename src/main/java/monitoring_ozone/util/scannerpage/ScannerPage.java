83c8e4d3ba4603948d3fc505ff24893deb516e00
zotov88 <zotov_l88@mail.ru>
2023-10-31 10:29:04 +0300

    Добавил отправление отчета в тг, если произошла ошибка в сканировании карточки товара

--- src/main/java/monitoring_ozone/util/scannerpage/ScannerPage.java
+++ src/main/java/monitoring_ozone/util/scannerpage/ScannerPage.java
@@ -1,5 +1,6 @@
 package monitoring_ozone.util.scannerpage;
 
+import monitoring_ozone.constants.Errors;
 import monitoring_ozone.model.Product;
 import monitoring_ozone.util.notification.NotificationProxy;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,7 @@ public class ScannerPage {
     public Product scanPage(String url) {
         Product product = turningProduct.getProduct(url);
         if (product == null) {
-            notificationProxy.sendToAdmin("Ошибка при сохранении товара " + url);
+            notificationProxy.sendToAdmin(Errors.Message.PRODUCT_PROCESSING_ERROR + url);
         }
         return product;
     }
